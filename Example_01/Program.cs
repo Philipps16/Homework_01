c7e232156f0f4858d7c0a99b261f46f0a06932fc
Philipp Mitrofanov <press.fm@mail.ru>
2022-12-03 14:38:34 +0300

    Выполнил упражнение 01

---
+++ Example_01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32 (Console.ReadLine());
+int Maximal = 0;
+if (a > b)
+{
+    Maximal = a;
+}
+    else
+    {
+        Maximal = b;
+    }
+Console.WriteLine ("Максимальное число из двух: ");
+Console.WriteLine (Maximal);
