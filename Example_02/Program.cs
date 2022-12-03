f8c090c834d7c94ba231bbaae0fe0a559c7d7ea4
Philipp Mitrofanov <press.fm@mail.ru>
2022-12-03 14:48:11 +0300

    Выполнил упражнение 02

---
+++ Example_02/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = Convert.ToInt32 (Console.ReadLine());
+int Maximal = a;
+if (a > Maximal)
+{
+    Maximal = a;
+}
+    else if (b > Maximal)
+    {
+        Maximal = b;
+    }
+    if (c > Maximal)
+    {
+        Maximal = c;
+    }
+  
+Console.WriteLine ("Максимальное число из трех: ");
+Console.WriteLine (Maximal);
