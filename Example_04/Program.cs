05caee4f796d211f2e7685206e5af295998de269
Philipp Mitrofanov <press.fm@mail.ru>
2022-12-03 15:02:15 +0300

    Выполнил упражнение 04

---
+++ Example_04/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear ();
+Console.WriteLine ("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine ());
+int a = 1;
+while (a <= N)
+{
+if (a % 2 == 0)
+{
+    Console.Write (a);
+    Console.Write (",");
+}
+a = a + 1;
+}
